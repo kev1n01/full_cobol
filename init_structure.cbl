ea2d9815d6e4d8c25c06cb8d1477d616006e0e10
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-06-23 18:19:08 -0500

    init structure to programa cobol

---
+++ init_structure.cbl
@@ -0,0 +1,26 @@
+      ******************************************************************
+      *  Author: Kevin Arnold 
+      *  Date: 23/06/2025
+      *  Purpose: Introduction
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. P0392.
+       AUTHOR. KEVIN ARNOLD.
+       DATE-WRITTEN. 23/06/2025.
+       
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       
+       DATA DIVISION.
+       FILE SECTION.       
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+           DISPLAY 'Area de ejecución'.
+           STOP RUN.
+       END PROGRAM P0392.
+
+
