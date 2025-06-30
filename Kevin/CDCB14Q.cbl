2f7007477a54a8e59e88f1439cd47f87c6c742a7
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-06-30 11:48:23 -0500

    PRUEBA TEST KEVIN

---
+++ Kevin/CDCB14Q.cbl
@@ -0,0 +1,344 @@
+************************************************************************
+******* 512972 16/05/25 ARC HOMOLOGACIÓN DE CAMPOS PARA TERADATA       *
+******* 413119 16/12/24 TEC INNOMINADAS-INTERFAZ DATA REPORTERIA FASE 2*
+*OBJET******************************************************************
+*OBJET*** MATCHING X PAN INNOMINADA                                    *
+*OBJET******************************************************************
+
+       IDENTIFICATION DIVISION.
+      *========================*
+       PROGRAM-ID.    CDCB14Q.
+       AUTHOR.        OLSSA.
+       DATE-WRITTEN.  DIC 2024.
+
+       ENVIRONMENT DIVISION.
+      *======================*
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+
+           SELECT  FTOTT09I    ASSIGN  TO  FTOTT09I
+                                   FILE STATUS IS FS-FTOTT09I.
+
+           SELECT  FFECACTI    ASSIGN  TO  FFECACTI
+                                   FILE STATUS IS FS-FFECACTI.
+
+           SELECT  FECACTVO    ASSIGN  TO  FECACTVO
+                                   FILE STATUS IS FS-FECACTVO.
+
+      *=============*
+       DATA DIVISION.
+      *=============*
+      *=============*
+       FILE SECTION.
+      *=============*
+      *SOLICITUDES PROCESADAS(ESTADO 1)
+       FD  FTOTT09I
+           LABEL RECORD IS STANDARD
+           BLOCK CONTAINS 0 RECORDS
+           RECORDING MODE IS F.
+       01 REG-DATAREPO           PIC X(310).
+           COPY CDCFDREP.
+           02 CDC-NUMPLAST       PIC 9(12).
+           02 CDC-NUMBEN         PIC 9(05).
+
+       FD  FFECACTI
+           LABEL RECORD IS STANDARD
+           BLOCK CONTAINS 0 RECORDS
+           RECORDING MODE IS F.
+       01  REG-FFECACTI.
+           02 CDC-FECACTI   PIC X(08).
+           02 CDC-PANINNO   PIC X(21).
+
+       FD  FECACTVO
+           LABEL RECORD IS STANDARD
+           BLOCK CONTAINS 0 RECORDS
+           RECORDING MODE IS F.
+       01  REG-SALIDA             PIC X(312).
+           COPY CDCFDREP REPLACING LEADING ==CDC== BY ==REP==.
+           02 REP-NUMPLAST        PIC 9(12).
+           02 REP-NUMBEN          PIC 9(05).
+           02 REP-LINEVENT        PIC X(02).
+
+      *========================*
+       WORKING-STORAGE SECTION.
+      *========================*
+       01 WSV-VARIABLES.
+          05 WSV-FSTATUS         PIC  9(02)  VALUE ZEROS.
+          05 WSV-RUTINA          PIC  X(18)  VALUE SPACES.
+          05 WSV-ACCION          PIC  X(18)  VALUE SPACES.
+
+       01 WSF-FSTATUS.
+          05 FS-FTOTT09I         PIC  X(02)  VALUE '00'.
+          05 FS-FFECACTI         PIC  X(02)  VALUE '00'.
+          05 FS-FECACTVO         PIC  X(02)  VALUE '00'.
+512972    05 WS-MATCH            PIC  X     VALUE ' '.
+512972       88 SW-MATCH-SI               VALUE 'S'.
+512972    05 WS-NO-MATCH         PIC  X     VALUE ' '.
+512972       88 SW-NO-MATCH-SI             VALUE 'S'.
+512972*    05 WS-GRABAR         PIC  X     VALUE ' '.
+512972*       88 SW-GRABAR-SI             VALUE 'S'.
+
+       01 WSC-COSTANTES.
+          05 WSC-10              PIC  9(02)  VALUE 10.
+          05 WSC-00              PIC  9(02)  VALUE 00.
+          05 WSC-16              PIC  9(02)  VALUE 16.
+
+       01 WSA-ACUMULADORES.
+          05 WSV-CONT-ACFI1      PIC  9(07) COMP
+                                             VALUE ZEROS.
+          05 WSV-CONT-ACFI2      PIC  9(07) COMP
+                                             VALUE ZEROS.
+          05 WSV-CONT-ACFO1      PIC  9(07) COMP
+                                             VALUE ZEROS.
+
+       01 WSS-SWITCH.
+          05 WS-FIN-FTOTT09I     PIC  9(01)  VALUE 0.
+             88 FIN-FTOTT09I-OK              VALUE 1.
+          05 WS-FIN-FFECACTI     PIC  9(01)  VALUE 0.
+             88 FIN-FFECACTI-OK              VALUE 1.
+
+       01 FEC-ACT1             PIC X(10)       VALUE "ACTIVACION".
+       01 FEC-ACT2             PIC X(10)       VALUE ALL SPACES.
+       01 FEC-SPAC             PIC X(8)        VALUE ALL SPACES.
+
+       01 TIP-VEN1            PIC X(02)       VALUE "P".
+       01 TIP-VEN2            PIC X(02)       VALUE "NP".
+
+      *-----------------------+
+      * VARAIBLES DE FECHAS   +
+      *-----------------------+
+       COPY COMWTIME.
+
+       01 OUT-DATE.
+          02 OUT-DIA             PIC 9(02).
+          02 FILLER              PIC X(01)   VALUE '/'.
+          02 OUT-MES             PIC 9(02).
+          02 FILLER              PIC X(01)   VALUE '/'.
+          02 OUT-SIGANO.
+             04 OUT-SIG          PIC 9(02).
+             04 OUT-ANO          PIC 9(02).
+
+       01 HORAMVS.
+          05 HH                  PIC 9(02).
+          05 FILLER              PIC X(01)   VALUE ':'.
+          05 MM                  PIC 9(02).
+          05 FILLER              PIC X(01)   VALUE ':'.
+          05 SS                  PIC 9(02).
+
+           COPY COMWUPSI.
+      *----------------------------------------------------------------*
+      *=================*
+       LINKAGE SECTION.
+      *=================*
+            COPY COMWLUPS.
+      *----------------------------------------------------------------*
+      *===============================*
+       PROCEDURE DIVISION USING LKUPSI.
+      *===============================*
+           PERFORM 1000-INICIO-PROGRAMA
+           PERFORM 2000-PROCESO-PROGRAMA
+           PERFORM 3000-FIN-PROGRAMA.
+      *----------------------------------------------------------------*
+      *====================*
+       1000-INICIO-PROGRAMA.
+      *====================*
+           PERFORM 1001-OPEN-FILES
+           PERFORM 1003-READ-FTOTT09I
+           PERFORM 1002-READ-FFECACTI
+           PERFORM 1004-OBTENER-FECHA.
+      *----------------------------------------------------------------*
+      *===============*
+       1001-OPEN-FILES.
+      *===============*
+
+           OPEN INPUT FTOTT09I FFECACTI
+                OUTPUT FECACTVO
+
+           IF (FS-FTOTT09I = '00' OR '97') AND
+              (FS-FFECACTI = '00' OR '97') AND
+              (FS-FECACTVO = '00' OR '97')
+              CONTINUE
+           ELSE
+              DISPLAY ' ERROR AL ABRIR ARCHIVOS  '
+              DISPLAY ' FS-FTOTT09I ............. = ' FS-FTOTT09I
+              DISPLAY ' FS-FFECACTI ............. = ' FS-FFECACTI
+              DISPLAY ' FS-FECACTVO ............. = ' FS-FECACTVO
+              PERFORM  9000-ERROR-PGM
+           END-IF.
+
+      *----------------------------------------------------------------*
+      *===================*
+       1003-READ-FTOTT09I.
+      *===================*
+           READ FTOTT09I
+           AT END
+              SET FIN-FTOTT09I-OK TO TRUE
+              MOVE '9999999999999999999999' TO CDC-PAN
+           END-READ.
+           EVALUATE FS-FTOTT09I
+           WHEN WSC-00
+                ADD 1 TO WSV-CONT-ACFI1
+           WHEN WSC-10
+                SET FIN-FTOTT09I-OK TO TRUE
+           WHEN OTHER
+                MOVE '1003-READ-FTOTT09I' TO WSV-RUTINA
+                MOVE 'READ FTOTT09I' TO WSV-ACCION
+                MOVE FS-FTOTT09I TO WSV-FSTATUS
+                PERFORM 9000-ERROR-PGM
+           END-EVALUATE.
+      *----------------------------------------------------------------*
+      *===================*
+       1002-READ-FFECACTI.
+      *==================*
+           READ FFECACTI
+           AT END
+              SET FIN-FFECACTI-OK TO TRUE
+              MOVE '9999999999999999999999' TO CDC-PANINNO
+           END-READ.
+           EVALUATE FS-FFECACTI
+           WHEN WSC-00
+                ADD 1 TO WSV-CONT-ACFI2
+           WHEN WSC-10
+                SET FIN-FFECACTI-OK TO TRUE
+           WHEN OTHER
+                MOVE '1004-READ-FFECACTI' TO WSV-RUTINA
+                MOVE 'READ FFECACTI' TO WSV-ACCION
+                MOVE FS-FFECACTI TO WSV-FSTATUS
+                PERFORM 9000-ERROR-PGM
+           END-EVALUATE.
+      *----------------------------------------------------------------*
+      *===================*
+       1004-OBTENER-FECHA.
+      *==================*
+           COPY COMLUPSI.
+           COPY COMLTIME.
+      *--- HORA Y FECHA DEL SISTEMA
+           DISPLAY 'HORA  DEL SISTEMA : ' HORAMVS
+           DISPLAY 'FECHA DEL SISTEMA : ' OUT-DATE.
+      *----------------------------------------------------------------*
+      *=====================*
+       2000-PROCESO-PROGRAMA.
+      *=====================*
+           PERFORM 2001-MATCHING-X-PAN UNTIL FIN-FTOTT09I-OK.
+      *----------------------------------------------------------------*
+
+      *=================*
+       2001-MATCHING-X-PAN.
+      *=================*
+           MOVE SPACES TO WS-MATCH
+           MOVE SPACES TO WS-NO-MATCH
+512972*    MOVE SPACES TO WS-GRABAR
+
+           EVALUATE TRUE
+           WHEN CDC-PAN = CDC-PANINNO
+                SET SW-MATCH-SI TO TRUE
+512972*         SET SW-GRABAR-SI TO TRUE
+                PERFORM 2002-MOVER-DATOS
+                PERFORM 1003-READ-FTOTT09I
+                PERFORM 1002-READ-FFECACTI
+           WHEN CDC-PAN < CDC-PANINNO
+                SET SW-NO-MATCH-SI TO TRUE
+512972*         SET SW-GRABAR-SI TO TRUE
+                PERFORM 2002-MOVER-DATOS
+                PERFORM 1003-READ-FTOTT09I
+           WHEN CDC-PAN > CDC-PANINNO
+512972*         SET SW-NO-MATCH-SI TO TRUE
+512972*         SET SW-GRABAR-SI TO TRUE
+512972*         PERFORM 2002-MOVER-DATOS
+                PERFORM 1002-READ-FFECACTI
+           END-EVALUATE.
+      *----------------------------------------------------------------*
+      *==================*
+       2002-MOVER-DATOS.
+      *==================*
+           INITIALIZE REG-SALIDA
+           MOVE REG-DATAREPO TO REG-SALIDA
+
+           IF SW-MATCH-SI
+                MOVE CDC-FECACTI  TO REP-FEC-ACUSE-TAR
+                MOVE FEC-ACT1     TO REP-TEC-TARJ
+                MOVE TIP-VEN2     TO REP-LINEVENT
+512972          PERFORM 2004-GRABAR-SALIDA
+           END-IF
+           IF SW-NO-MATCH-SI
+                MOVE TIP-VEN1 TO REP-LINEVENT
+                MOVE FEC-ACT2 TO REP-TEC-TARJ
+                MOVE FEC-SPAC TO REP-FEC-ACUSE-TAR
+512972          PERFORM 2004-GRABAR-SALIDA
+           END-IF
+512972*    IF SW-GRABAR-SI
+512972*         PERFORM 2004-GRABAR-SALIDA
+512972*    END-IF
+           .
+      *----------------------------------------------------------------*
+      *==================*
+       2004-GRABAR-SALIDA.
+      *==================*
+           WRITE REG-SALIDA
+           IF FS-FECACTVO NOT = '00'
+              MOVE '2003-SAVE-FECACTVO   ' TO WSV-RUTINA
+              MOVE 'WRITE REG-FECACTVO   ' TO WSV-ACCION
+              MOVE FS-FECACTVO TO WSV-FSTATUS
+              PERFORM 9000-ERROR-PGM
+           END-IF
+           ADD 1 TO WSV-CONT-ACFO1.
+      *----------------------------------------------------------------*
+      *=================*
+       3000-FIN-PROGRAMA.
+      *=================*
+           PERFORM 3001-CLOSE-FILES
+           PERFORM 3002-CARGA-DETALLES
+           STOP RUN.
+      *----------------------------------------------------------------*
+      *================*
+       3001-CLOSE-FILES.
+      *================*
+           CLOSE FTOTT09I FFECACTI FECACTVO
+
+           IF (FS-FTOTT09I = '00') AND
+              (FS-FFECACTI = '00') AND
+              (FS-FECACTVO = '00')
+              CONTINUE
+           ELSE
+              DISPLAY ' ERROR AL ABRIR ARCHIVOS  '
+              DISPLAY ' FS-FTOTT09I ............. = ' FS-FTOTT09I
+              DISPLAY ' FS-FFECACTI ............. = ' FS-FFECACTI
+              DISPLAY ' FS-FECACTVO ............. = ' FS-FECACTVO
+              PERFORM  9000-ERROR-PGM
+           END-IF.
+      *----------------------------------------------------------------*
+      *===================*
+       3002-CARGA-DETALLES.
+      *===================*
+           DISPLAY '================================='
+           DISPLAY '------- DETALLES PROCESO --------'
+           DISPLAY '-------     CDCB14Q      --------'
+           DISPLAY '================================='
+           DISPLAY 'REG. LEIDOS FTOTT09I = ' WSV-CONT-ACFI1
+           DISPLAY 'REG. LEIDOS FFECACTI = ' WSV-CONT-ACFI2
+           DISPLAY 'REG. GRABAD FECACTVO = ' WSV-CONT-ACFO1
+           DISPLAY '================================='
+           MOVE WSC-00 TO RETURN-CODE
+           DISPLAY '================================='
+           DISPLAY '--------- FIN DETALLES ----------'
+           DISPLAY '================================='.
+      *----------------------------------------------------------------*
+      *==============*
+       9000-ERROR-PGM.
+      *==============*
+           DISPLAY '================================'
+           DISPLAY '------ DETALLES DE ERROR -------'
+           DISPLAY '------      CDCB14Q      -------'
+           DISPLAY '================================'
+           DISPLAY ' RUTINA          :' WSV-RUTINA
+           DISPLAY ' ACCION DE ERROR :' WSV-ACCION
+           DISPLAY ' CODIGO DE ERROR :' WSV-FSTATUS
+           DISPLAY '================================'
+           DISPLAY '--------- FIN DETALLES ---------'
+           DISPLAY '================================'
+           MOVE WSC-16 TO RETURN-CODE
+
+           STOP RUN.
+      *----------------------------------------------------------------*
+
---
+++ Kevin/CDCFDREP.cpy
@@ -0,0 +1,93 @@
+***********************************************************************
+*     * 412213 17/06/24 ACO INNOMINADAS-INTERFAZ DATA REPORTERIA      *
+***********************************************************************
+      *-------------------------------------------------------------*
+      *    COPY     : CDCFDREP                                      *
+      *    REGISTRO : ARCHIVOS PARA DATA REPORTERÍA                 *
+      *    LONGITUD : 293 BYTES                                     *
+      *-------------------------------------------------------------*
+       01  REG-CDCFDREP.
+      * CODIGO UNICO TITULAR
+           03  CDC-CU-TIT         PIC 9(10) VALUE ZEROES.
+           03  CDC-FILLER01       PIC X(01).
+      * CODIGO UNICO BENEFICIARIO
+           03  CDC-CU-ADI         PIC 9(10) VALUE ZEROES.
+           03  CDC-FILLER02       PIC X(01).
+      * NOMBRE DEL CLIENTE
+           03  CDC-NOM-CLIENTE    PIC X(40).
+           03  CDC-FILLER03       PIC X(01).
+      * TIPO DE CLIENTE
+           03  CDC-TIPO-CLIENTE   PIC X(04).
+           03  CDC-FILLER04       PIC X(01).
+      * MARCA-TIPO
+           03  CDC-MARCA-TIPO.
+               05 CDC-MARCA       PIC 9(02).
+               05 CDC-TIPO        PIC 9(02).
+           03  CDC-FILLER05       PIC X(01).
+      * TIPO DE DOCUMENTO
+           03  CDC-TIPO-DOC       PIC X(01).
+           03  CDC-FILLER06       PIC X(01).
+      * NUMERO DE DOCUMENTO
+           03  CDC-NUM-DOC        PIC X(11).
+           03  CDC-FILLER07       PIC X(01).
+      * NUMERO DE CUENTA
+           03  CDC-NUM-CTA        PIC 9(12).
+           03  CDC-FILLER08       PIC X(01).
+      * NUMERO DE PAN
+           03  CDC-PAN            PIC X(22).
+           03  CDC-FILLER09       PIC X(01).
+      * ALIAS
+           03  CDC-ALIAS          PIC X(22).
+           03  CDC-FILLER10       PIC X(01).
+      * PAN TRUNCADO
+           03  CDC-PAN-TRCD       PIC X(22).
+           03  CDC-FILLER11       PIC X(01).
+      * ID SOLICITUD
+           03  CDC-SOL-PRODUCTO   PIC X(20).
+           03  CDC-FILLER12       PIC X(01).
+      * ESTADO SOLICITUD
+           03  CDC-EST-SOL        PIC X(01).
+           3   CDC-FILLER13       PIC X(01).
+      * MOTIVO BAJA CTA
+           03  CDC-MOTBAJA-CTA    PIC X(02).
+           03  CDC-FILLER14       PIC X(01).
+      * FECHA BAJA CTA
+           03  CDC-FECBAJA-CTA    PIC 9(08).
+           03  CDC-FILLER15       PIC X(01).
+      * FECHA ALTA CTA
+           03  CDC-FECALTA-CTA    PIC 9(08).
+           03  CDC-FILLER16       PIC X(01).
+      * SITUACION CTA
+           03  CDC-SIT-CTA        PIC X(02).
+           03  CDC-FILLER17       PIC X(01).
+      * COD BLOQUEO TARJ
+           03  CDC-CODBLOQ-TAR    PIC 9(02).
+           03  CDC-FILLER18       PIC X(01).
+      * FECHA BLOQ TARJ
+           03  CDC-FECBLOQ-TAR    PIC 9(08).
+           03  CDC-FILLER19       PIC X(01).
+      * FLUJO ALTA TARJ
+           03  CDC-FLUJO-ALT-TAR  PIC X(15).
+           03  CDC-FILLER20       PIC X(01).
+      * CANAL ALT TARJ
+           03  CDC-CANAL-ALT-TAR  PIC X(10).
+           03  CDC-FILLER21       PIC X(01).
+      * FECHA ALT TARJ
+           03  CDC-FEC-ALT-TAR    PIC 9(08).
+           03  CDC-FILLER22       PIC X(01).
+      * FECHA ACUSE TARJ
+           03  CDC-FEC-ACUSE-TAR  PIC 9(08).
+           03  CDC-FILLER23       PIC X(01).
+      * TECNOLOGIA DE LA TARJETA
+           03  CDC-TEC-TARJ       PIC X(10).
+           03  CDC-FILLER24       PIC X(01).
+      * SITUACION TARJETA
+           03  CDC-SIT-TAR        PIC 9(02).
+           03  CDC-FILLER25       PIC X(01).
+      * TIPO DE TARJETA
+           03  CDC-TIPO-TAR       PIC X(03).
+           03  CDC-FILLER26       PIC X(01).
+      * CAMBIO DE PIN
+           03  CDC-CAMBIO-PIN     PIC X(01).
+           03  CDC-FILLER27       PIC X(01).
+      *-------------------------------------------------------------*
