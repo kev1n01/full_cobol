6b980566b910354cd9617a98b8f85de41a37c0b4
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-07-01 16:29:55 -0500

    Add initial COBOL program for Behance IBM calculation

---
+++ caso_02/behance_ibm_calculate.cbl
@@ -0,0 +1,26 @@
+      ******************************************************************
+      *  Author: Kevin Arnold 
+      *  Date: 30/06/2025
+      *  Purpose: Behance ibm calculate with file
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. P0IBMFILE.
+       AUTHOR. KEVIN ARNOLD.
+       DATE-WRITTEN. 23/06/2025.
+       
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       
+       DATA DIVISION.
+       FILE SECTION.       
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+           DISPLAY 'Area de ejecución'.
+           STOP RUN.
+       END PROGRAM P0IBMFILE.
+
+
